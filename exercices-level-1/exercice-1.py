a82b4c6ab1b7564eab2f95fac4297ce3e80ec1e1
TomasGonlez <tomas.gonzalez.f@alumnos.upla.cl>
2024-05-07 02:19:35 -0400

    ej_1

--- exercices-level-1/exercice-1.py
+++ exercices-level-1/exercice-1.py
@@ -1,3 +1,13 @@
 '''Definir una función max() que tome como argumento dos números y devuelva el mayor de ellos.
 Es cierto que python tiene una función max() incorporada, pero hacerla nosotros mismos es un
 muy buen ejercicio'''
+
+
+def max(a,b):
+    if a < b:
+        return b
+    if b < a:
+        return a
+print(max(100,101))
+
+
